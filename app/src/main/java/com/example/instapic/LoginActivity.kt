82491c1c381be3c7f71726dc6e7fdf549c101748
Woolygan <pogidariusgarcia@gmail.com>
2022-02-09 23:59:43 -0800

    Set up Parse and Back4App

---
+++ app/src/main/java/com/example/instapic/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.example.instapic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.login_button).setOnClickListener {
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            loginUser(username, password)
+        }
+    }
+
+    private fun loginUser (username: String, password: String) {
+
+    }
+}
--- app/src/main/java/com/example/instapic/MainActivity.kt
+++ app/src/main/java/com/example/instapic/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.instapic
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+// Let user create a post by taking a photo with their camera.
+*/
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
